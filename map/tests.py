83b4f8cc58bebd29395259ad04a96f2f305ee8ec
Zelknite <rgk6pa@virginia.edu>
2021-04-11 11:16:15 -0400

    Added Navigation to the Map

--- map/tests.py
+++ map/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .views import views
+from django.contrib.auth.models import User
 
-# Create your tests here.
+#
+# def test_html(self):
+#     self.assertContains(self.response, '<form')
+#     self.assertContains(self.response, 'type="hidden"', 1)
+#     self.assertContains(self.response, 'type="text"', 2)
+#     self.assertContains(self.response, 'type="radio"', 3)
+#     self.assertContains(self.response, 'type="submit"', 1)
