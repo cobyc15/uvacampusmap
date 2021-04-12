feee544925a8916c9b5d3c545916d7bc3ad7bd2a
Zelknite <rgk6pa@virginia.edu>
2021-04-11 22:47:48 -0400

    Added Navigation to the Map

--- map/tests.py
+++ map/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
-from .views import views
-from django.contrib.auth.models import User
+#from .views import views
+#from django.contrib.auth.models import User
 
 #
 # def test_html(self):
