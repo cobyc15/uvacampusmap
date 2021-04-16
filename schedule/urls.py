79c1712228ceb1c6e42c2a8cedddc02b9b2dabd2
Matthew Gerace <matthewjgerace@gmail.com>
2021-04-16 18:08:08 -0400

    Schedule With Events

--- schedule/urls.py
+++ schedule/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'schedule'
 urlpatterns = [
     url(r'^index/$', views.index, name='index'),
-    url(r'^schedule/$', views.CalendarView.as_view(), name='schedule')
+    url(r'^schedule/$', views.CalendarView.as_view(), name='schedule'),
+    url(r'^event/new/$', views.event, name='event_new'),
+    url(r'^event/edit/(?P<event_id>\d+)/$', views.event, name='event_edit'),
 ]
--- schedule/views.py
+++ schedule/views.py
@@ -1,15 +1,14 @@
+from datetime import timedelta
+from datetime import date
+import calendar
 from django.shortcuts import render
 from django.http import HttpResponse
-
-# Create your views here.
-
-from datetime import datetime
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.views import generic
 from django.utils.safestring import mark_safe
-
+from django.shortcuts import get_object_or_404
 from .models import *
+from .forms import EventForm
 from .utils import Calendar
 
 def index(request):
@@ -18,16 +17,12 @@ def index(request):
 class CalendarView(generic.ListView):
     model = Event
     template_name = 'schedule/schedule.html'
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-
-        # use today's date for the calendar
-        d = get_date(self.request.GET.get('day', None))
-
-        # Instantiate our calendar class with today's year and date
+        d = get_date(self.request.GET.get('month', None))
+        context['prev_month'] = prev_month(d)
+        context['next_month'] = next_month(d)
         schedule = Calendar(d.year, d.month)
-
         # Call the formatmonth method, which returns our calendar as a table
         html_schedule = schedule.formatmonth(withyear=True)
         context['calendar'] = mark_safe(html_schedule)
@@ -37,4 +32,30 @@ def get_date(req_day):
     if req_day:
         year, month = (int(x) for x in req_day.split('-'))
         return date(year, month, day=1)
-    return datetime.today()
+    return date.today()
+
+def prev_month(d):
+    first = d.replace(day=1)
+    prev_month = first - timedelta(days=1)
+    month = 'month=' + str(prev_month.year) + '-' + str(prev_month.month)
+    return month
+
+def next_month(d):
+    days_in_month = calendar.monthrange(d.year, d.month)[1]
+    last = d.replace(day=days_in_month)
+    next_month = last + timedelta(days=1)
+    month = 'month=' + str(next_month.year) + '-' + str(next_month.month)
+    return month
+
+def event(request, event_id=None):
+    instance = Event()
+    if event_id:
+        instance = get_object_or_404(Event, pk=event_id)
+    else:
+        instance = Event()
+    
+    form = EventForm(request.POST or None, instance=instance)
+    if request.POST and form.is_valid():
+        form.save()
+        return HttpResponseRedirect(reverse('schedule:schedule'))
+    return render(request, 'schedule/event.html', {'form': form})
