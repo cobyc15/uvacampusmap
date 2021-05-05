126136e6c4818daf44ac09a142c82647fd963ff7
RjK <77639105+Zelknite@users.noreply.github.com>
2021-05-05 18:54:18 -0400

    Update views.py
    citations

--- CommunitySchedule/views.py
+++ CommunitySchedule/views.py
@@ -85,3 +85,8 @@ def communityevent(request, event_id=None):
             except:
                 return render(request, 'CommunitySchedule/event2.html', {'form': form})
     return render(request, 'CommunitySchedule/event2.html', {'form': form})
+
+# Hui Wen
+# Date made: 24 July 2018
+# Web browser Calendar source code
+# https://www.huiwenteo.com/normal/2018/07/24/django-calendar.html 
